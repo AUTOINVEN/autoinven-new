c6022821498621b8852dd073cb6f548b41cce6ac
munbbok <rofeels@gmail.com>
2023-03-07 14:34:56 +0900

    kakao login

--- web/routes/api/auth/kakao/index.js
+++ web/routes/api/auth/kakao/index.js
@@ -1,66 +1,106 @@
-module.exports = (db) => {
+module.exports = (db,session) => {
   const express = require('express');
   const router = express.Router();
+  const qs = require('querystring');
 
   const { doAsync } = require('$base/utils/asyncWrapper');
+  const getEncryptedPasswordInfo = require('../signup/function/getEncryptedPasswordInfo');
+
 
   //카카오
   router.get(
     '/',
     doAsync(async (req, res) => {
-      const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile,account_email`;
+      const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${kakao.clientID}&redirect_uri=${kakao.redirectUri}&response_type=code&scope=profile_nickname,account_email`;
       res.redirect(kakaoAuthURL);
     })
   );
 
   const kakao = {
-    clientID: 'a78cff9fe4bee234ad577510e6a95d1d',
-    clientSecret: '클라이언트 SECRET키',
-    redirectUri: 'http://localhost:5000/api/auth/kakao/callback'
+    clientID: '09d6906c044decaac37d6caeb38b1892',
+    clientSecret: 'OeaFnLBgU8OLXCtrX3pid1gPZT1Rnlp1',
+    //redirectUri: 'http://localhost:5000/api/auth/kakao/callback'
+    redirectUri: 'http://autoinven.com/api/auth/kakao/callback'
   }
 
   //카카오 콜백
-  router.post(
+  router.get(
     '/callback',
     doAsync(async (req, res) => {
-      try{//access토큰을 받기 위한 코드
-        token = await axios({//token
-            method: 'POST',
-            url: 'https://kauth.kakao.com/oauth/token',
-            headers:{
-                'content-type':'application/x-www-form-urlencoded'
-            },
-            data:qs.stringify({
-                grant_type: 'authorization_code',//특정 스트링
-                client_id:kakao.clientID,
-                //client_secret:kakao.clientSecret,
-                redirectUri:kakao.redirectUri,
-                code:req.query.code,//결과값을 반환했다. 안됐다.
-            })//객체를 string 으로 변환
-        })
-      }catch(err){
-          res.json(err.data);
-      }
-      //access토큰을 받아서 사용자 정보를 알기 위해 쓰는 코드
-          let user;
-          try{
-              console.log(token);//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
-              user = await axios({
-                  method:'get',
-                  url:'https://kapi.kakao.com/v2/user/me',
-                  headers:{
-                      Authorization: `Bearer ${token.data.access_token}`
-                  }//헤더에 내용을 보고 보내주겠다.
-              })
-          }catch(e){
-              res.json(e.data);
-          }
-          console.log(user);
+      token = await fetch('https://kauth.kakao.com/oauth/token',{//token
+        method: 'POST',
+        headers:{
+            'content-type':'application/x-www-form-urlencoded'
+        },
+        body:qs.stringify({
+            grant_type: 'authorization_code',//특정 스트링
+            client_id:kakao.clientID,
+            client_secret:kakao.clientSecret,
+            redirectUri:kakao.redirectUri,
+            code:req.query.code,//결과값을 반환했다. 안됐다.
+        })//객체를 string 으로 변환
+      }).then((response) => response.json())
+      console.log('kakao token : ',token);
       
-          req.session.kakao = user.data;
-          //req.session = {['kakao'] : user.data};
+      let role = 'user';
+      let user;//access정보를 가지고 또 요청해야 정보를 가져올 수 있음.
+      try{
           
-          res.send('success');
+          user = await fetch('https://kapi.kakao.com/v2/user/me',{
+              method:'get',
+              headers:{
+                  Authorization: `Bearer ${token.access_token}`
+              }//헤더에 내용을 보고 보내주겠다.
+          }).then((response) => response.json())
+      }catch(e){
+          res.json(e.data);
+      }
+      console.log(user);
+  
+      if(user) req.session.kakao = user;
+      //req.session = {['kakao'] : user.data};
+
+      let member = await db.User.findOne({
+        where: {
+          email : user.kakao_account.email,
+          is_valid: 1,
+        },
+      });
+      if (!member) {
+        try {
+          password_info = await getEncryptedPasswordInfo(user.id.toString());
+          await db.User.create({
+            email : user.kakao_account.email,
+            name : user.kakao_account.profile.nickname,
+            phone : '',
+            salt:'',
+            ...password_info,
+          });
+      
+          // 세션에 데이터 저장
+          req.session.email = user.kakao_account.email;
+          req.session.role = 'user';
+          req.session.name = user.kakao_account.profile.nickname;
+          req.session.phone = '';
+          req.session.save(error => {if(error) console.log(error)})
+        } catch (err) {
+          throw err;
+        }
+        role = 'user';
+      }else{
+        try{
+          req.session.email = req.session.kakao.kakao_account.email;
+          req.session.role = 'user';
+          req.session.name = req.session.kakao.kakao_account.profile.nickname;
+          req.session.phone = '';
+          req.session.save(error => {if(error) console.log(error)})
+        } catch (err) {
+          throw err;
+        }
+      }
+      
+      //res.send('success');
+      res.redirect('/')
     })
   );
 
